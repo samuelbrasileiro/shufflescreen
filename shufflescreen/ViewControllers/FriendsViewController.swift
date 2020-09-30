131223e77e0f7be3168396d3bda3bf01b4a3e4be
Samuel <samuelbsantosn@gmail.com>
2020-09-30 01:05:37 -0300

    Finalizando criação das playlists entre amigos.

--- shufflescreen/ViewControllers/FriendsViewController.swift
+++ shufflescreen/ViewControllers/FriendsViewController.swift
@@ -22,7 +22,7 @@ class FriendsViewController: BaseViewController{
         child?.view.backgroundColor = .clear
         child?.view.translatesAutoresizingMaskIntoConstraints = true
         child?.overrideUserInterfaceStyle = .dark
-        child?.view.frame = CGRect(x: self.view.bounds.midX - 200, y: 90, width: 400, height: self.view.bounds.height - 50 - 30)
+        child?.view.frame = CGRect(x: self.view.bounds.midX - 200, y: 90, width: 400, height: self.view.bounds.height - 90 - 30)
         self.view.addSubview(child!.view)
     }
     
@@ -40,7 +40,21 @@ class FriendsBank: ObservableObject{
     
     @Published var friendDoesNotExist = false
     
+    @Published var didPostInSpotify = false
+    
+    @Published var items: [TopItem]?
+    
+    var playlistID: String?
+    
     var myCode: String
+    
+    var hasCreated: (Bool,Bool) = (false,false){
+        didSet{
+            if hasCreated == (true,true){
+                hasGenerated = true
+            }
+        }
+    }
     
     @Published var friendCode: String = ""{
         didSet{
@@ -62,6 +76,7 @@ class FriendsBank: ObservableObject{
     
     init(){
         self.myCode = defaults.string(forKey: Keys.kICloudCode)!
+        items = []
     }
     
     func generatePlaylist(){
@@ -98,26 +113,74 @@ class FriendsBank: ObservableObject{
         }
     }
     
+
+    
     func downloadTopItems(){
+        items = []
+        
         let myArtistsIDs = self.myRecord!["artistsIDs"] as! [String]
         let myTracksIDs = self.myRecord!["trackIDs"] as! [String]
         
         let friendArtistsIDs = self.friendRecord!["artistsIDs"] as! [String]
         let friendTracksIDs = self.friendRecord!["trackIDs"] as! [String]
         
-        let interleavedArtists = zip(myArtistsIDs, friendArtistsIDs).flatMap { [$0, $1] }
-        let interleavedArtistsWithPriority = (0 ..< interleavedArtists.count).map{(interleavedArtists[$0], $0, 1)}
+        let artistsSeeds = sortPriorizer(array1: myArtistsIDs, array2: friendArtistsIDs).shuffled()
+        let tracksSeeds = sortPriorizer(array1: myTracksIDs, array2: friendTracksIDs).shuffled()
         
-        let normalizedArtists = interleavedArtistsWithPriority.reduce([String:(Int,Int)]()) {
+        self.hasCreated = (false,false)
+        
+        Recommendations.fetch(artists: Array(artistsSeeds[0...2]), tracks: Array(tracksSeeds[0...1]), genres: [], limit: "30"){ result in
+            if case .success(let recommendations) = result {
+                
+                for item in recommendations.tracks!{
+                    self.addItem(track: item)
+                }
+                
+                self.hasCreated.0 = true
+            }
+        }
+        Recommendations.fetch(artists: Array(artistsSeeds[3...4]), tracks: Array(tracksSeeds[2...4]), genres: [], limit: "30"){ result in
+            if case .success(let recommendations) = result {
+                
+                for item in recommendations.tracks!{
+                    self.addItem(track: item)
+                }
+                
+                self.hasCreated.1 = true
+            }
+            
+        }
+    }
+    
+    func addItem(track: Track){
+        let item = TopItem(name: track.name!, image: nil, uri: track.uri, id: track.id)
+        self.items!.append(item)
+        let index = self.items!.count - 1
+        SPTImage.fetch(scale: 64, images: track.album!.images!){ result in
+            if case .success(let image) = result{
+                if index < self.items!.count{
+                    self.items![index] = TopItem(name: item.name, image: image, uri: item.uri, id: item.id)
+                }
+            }
+            else{
+                print("eita po")
+            }
+        }
+    }
+    
+    func sortPriorizer(array1: [String], array2: [String])->[String]{
+        let interleaved = zip(array1, array2).flatMap { [$0, $1] }
+        let interleavedWithPriority = (0 ..< interleaved.count).map{(interleaved[$0], $0, 1)}
+        
+        let normalized = interleavedWithPriority.reduce([String:(Int,Int)]()) {
             var dict = $0
             dict[$1.0] = (dict[$1.0] ?? (0,0))
             dict[$1.0]!.0 += $1.1/2
-            print($1.1)
             dict[$1.0]!.1 += $1.2
             return dict
             }.map{$0}
 
-        let orderedArtists = normalizedArtists.sorted{
+        let ordered = normalized.sorted{
             if $0.value.1 == $1.value.1{
                 return $0.value.0 < $1.value.0
             }
@@ -125,12 +188,9 @@ class FriendsBank: ObservableObject{
                 return $0.value.1 > $1.value.1
             }
         }
-        print(orderedArtists)
-        
-        let myTracks = [Track]()
-        let friendTracks = [Track]()
-        
+        return [String](ordered.map{$0.key}[0..<5])
     }
+    
     
     func fetchFriendByCode(_ completion: @escaping (CKRecord?) -> ()){
         
@@ -152,7 +212,6 @@ class FriendsBank: ObservableObject{
                     
                     print(results!.count)
                     if results?.count == 1 {
-                        print(results!.count)
                         completion(results![0])
                     }
                     else{//anormalidade
@@ -170,7 +229,85 @@ class FriendsBank: ObservableObject{
         }
         
     }
+    func pushToSpotify(){
+        
+        let user = User.restore()
+        guard let id = user?.id else {
+            print("error ao fetch da id")
+            return
+        }
+        let myName = (myRecord!["name"] as! String).split(separator: " ").first!
+        let friendName = (friendRecord!["name"] as! String).split(separator: " ").first!
+        let playlist = PlaylistInput(name: "\(myName) and \(friendName)'s Shuffle")
+        
+        self.createNewPlaylist(id: id, playlist: playlist) { playlistOutput in
+            guard let playlistid = playlistOutput?.id else { return }
+            self.addSongs(id: playlistid) {
+                print("songs added")
+                self.playlistID = playlistid
+                self.didPostInSpotify = true
+                
+            }
+        }
+    }
+    
+    // MARK: - Create Playlist Requests
+    
+    func createNewPlaylist(id: String, playlist: PlaylistInput, completion: @escaping (PlaylistOutput?) -> Void) {
+        let defaults = UserDefaults(suiteName: "group.samuel.shufflescreen.app")!
 
+        let url = URL(string: "https://api.spotify.com/v1/users/\(id)/playlists")!
+        var request = URLRequest(url: url)
+        
+        request.setValue("Bearer " + defaults.string(forKey: Keys.kAccessToken)!, forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        do {
+            let data = try JSONEncoder().encode(playlist)
+            request.httpBody = data
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                guard let response = response as? HTTPURLResponse, let data = data else { return }
+                guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+                    print("statusCode should be 2##, but is \(response.statusCode)")
+                    print("response = \(response)")
+                    return
+                }
+                do {
+                    let playlist = try JSONDecoder().decode(PlaylistOutput.self, from: data)
+                    completion(playlist)
+                } catch {
+                    print("error decoding playlist id")
+                    completion(nil)
+                }
+            }.resume()
+        } catch {
+            print("error encoding playlist")
+            completion(nil)
+        }
+        
+    }
+    func addSongs(id: String, completion: @escaping () -> Void) {
+        let defaults = UserDefaults(suiteName: "group.samuel.shufflescreen.app")!
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.spotify.com"
+        components.path = "/v1/playlists/\(id)/tracks"
+        
+        let joinedTracksURIs = items!.map({$0.uri!}).joined(separator: ",")
+        components.queryItems = [URLQueryItem(name: "uris", value: joinedTracksURIs)]
+        var request = URLRequest(url: components.url!)
+        request.setValue("Bearer " + defaults.string(forKey: Keys.kAccessToken)!, forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let response = response as? HTTPURLResponse else { return }
+            guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            completion()
+        }.resume()
+    }
     
 }
 
@@ -212,7 +349,6 @@ struct FriendsView: View{
             TextField("", text: $bank.friendCode)
                 .font(.system(size: 32, weight: .bold, design: .rounded))
                 .textCase(.uppercase)
-                
                 .frame(width: 120, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .multilineTextAlignment(.center)
                 .padding()
@@ -235,12 +371,47 @@ struct FriendsView: View{
             .padding()
             
             
-            ScrollView{
-                
+            ScrollView {
+                LazyVStack(alignment: .leading, spacing: 8) {
+                    ForEach((0..<bank.items!.count), id: \.self){ index in
+                        HStack(alignment: .center, spacing: 6){
+                            Link(destination: URL(string: bank.items![index].uri!)!){
+                                (bank.items![index].image == nil ?
+                                    Image("spotify") : Image(uiImage: bank.items![index].image!))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(minWidth: 0, maxWidth: 40, minHeight: 0, maxHeight: 40, alignment: .leading)
+                                
+                                Text("\(index + 1). " + bank.items![index].name)
+                                    .lineLimit(2)
+                                    .font(.system(size: 28, weight: .bold, design: .rounded))
+                                    .foregroundColor(Color(.black))
+                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+                                    
+                                
+                            }
+                            Spacer(minLength: 10)
+                            Button(action: {
+                                bank.items!.remove(at: index)
+                            }){
+                                
+                                Image(systemName: "multiply.circle.fill")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(minWidth: 0, maxWidth: 20, minHeight: 0, maxHeight: 20, alignment: .leading)
+                                    .foregroundColor(Color(.black))
+                            }
+                            Spacer(minLength: 10)
+                        }
+                        Divider()
+                    }
+                }.background(Color(.systemOrange))
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
             }.background(Color(.red))
+            
             if bank.hasGenerated{
                 Button("Push to Spotify"){
-                    
+                    bank.pushToSpotify()
                 }
                 .padding(.horizontal, 10.0)
                 .padding()
@@ -257,6 +428,14 @@ struct FriendsView: View{
         }.background(Color.orange)
         .alert(isPresented: $bank.friendDoesNotExist) {
             Alert(title: Text("This code is invalid"), message: Text("Please write another one"), dismissButton: .default(Text("Got it!")))
+        }
+        .alert(isPresented: $bank.didPostInSpotify){
+            Alert(title: Text("Hey, your playlist is in Spotify"), message: Text("Do you want to open it?"), primaryButton: .cancel(), secondaryButton: .default(Text("Let's go!"), action: {
+                let url = URL(string: "spotify:playlist:\(bank.playlistID!)")!
+                if UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }))
         }
     }
     
